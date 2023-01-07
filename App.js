f2738c4052697434c81f5c401bc87e7b5cd54069
Shamim <md_shamim_cs@outlook.com>
2023-01-07 19:39:24 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Init ReactNative <Text style={styles.highlight}>App.js</Text>
+          </Section>
+          <Section title="Step Two">
+            Setup Environment export{'\n'}
+            <Text style={styles.highlight}>
+              export PATH=$PATH:$ANDROID_SDK_ROOT/emulator export
+              {'\n'}
+            </Text>{' '}
+            <Text style={styles.highlight}>
+              Setup Environment export
+              ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+              {'\n'}
+            </Text>
+            <Text style={styles.highlight}>
+              PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+              {'\n'}
+            </Text>
+          </Section>
+          <Section title="Step Three">
+            <Text style={styles.highlight}> npm run android</Text> {'\n'}
+            Push git on success.
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
